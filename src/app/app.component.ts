8106e7b128f669961e93a5c5ea45238dd6fbaa49
Miguel Chico <miguelchico@gmail.com>
2020-07-28 01:20:52 +0200

    add ng-keyboard-shortcuts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import {
+  ShortcutInput,
+  ShortcutEventOutput,
+  KeyboardShortcutsComponent,
+  AllowIn,
+} from 'ng-keyboard-shortcuts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'basic-angular-cli-universal';
+export class AppComponent implements AfterViewInit {
+  shortcuts: ShortcutInput[] = [];
+
+  ngAfterViewInit(): void {
+    this.shortcuts.push(
+      {
+        key: 'ctrl + t',
+        preventDefault: true,
+        allowIn: [AllowIn.Textarea, AllowIn.Input],
+        command: (e) => console.log('clicked ', e.key),
+      },
+      {
+        key: ['? a'],
+        label: 'Sequences',
+        description: 'Sequence ? and a',
+        command: (output: ShortcutEventOutput) => console.log('? a', output),
+        preventDefault: true,
+      }
+    );
+  }
 }
